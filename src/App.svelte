a37f165b99a5105a64839997fdfdbd8fd61b4104
Vladimir Ponomarenko <p.v.v1313@gmail.com>
2019-12-10 21:16:23 +0200

    SvelteJS

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let name;
+	export let age;
+
+	function changeAge() {
+		age = Math.floor(Math.random() * 10 + 30);
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click={changeAge}>Change Age</button>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'Volodymyr',
+		age: 36
+	}
+});
+
+export default app;
