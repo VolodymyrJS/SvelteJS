2085163181d5defce298b633a0d9bff6a00c268c
Vladimir Ponomarenko <p.v.v1313@gmail.com>
2019-12-15 23:01:39 +0200

    condition and two way binding

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,55 @@
 <script>
-	export let name;
-	export let age;
+  import ContactCard from "./ContactCard.svelte";
 
-	function changeAge() {
-		age = Math.floor(Math.random() * 10 + 30);
+  let name = "Max";
+  let title = "";
+  let image = "";
+	let description = "";
+	let checkStatus = false;
+	
+	function addCard() {
+		if(name.trim() == "" || title.trim() == "" || description.trim() == "" || image.trim() == "") {
+			checkStatus = "!checked";
+			return;
+		} else {
+			checkStatus = 'checked';
+		}
+		
 	}
+
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
 </style>
 
-<h1>Hello {name}, my age is {age}!</h1>
-<button on:click={changeAge}>Change Age</button>
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+{#if checkStatus == 'checked'}
+	<ContactCard userName={name} jobTitle={title} {description} userImage={image} />
+{:else if checkStatus == '!checked'}
+  <p>You don't fill all fields</p>
+{:else}
+	<p>Plese fill the fields.</p>
+{/if}
+<button on:click={addCard}>Add a card.</button>
+
